b51196dd361a687308f5b60ebe5f34eff7ccc82b
malsadev <malsa080@uottawa.ca>
2025-04-27 21:50:57 -0400

    implemented user sign in view

---
+++ src/components/SignIn.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { lemmyClient } from '@/services/lemmyService';
+import { ref } from 'vue';
+
+const username = ref('')
+const password = ref('')
+
+
+async function submit_sign_in() {
+    console.log("submit_sign_in")
+    const res = await lemmyClient.login({username_or_email: username.value,
+                                      password: password.value,
+    });
+
+    console.log(res)
+
+};
+
+</script>
+
+
+<template>
+    <h1>User Sign In</h1>
+    <form @submit.prevent="submit_sign_in">
+        <label>Username or Email</label>
+        <input v-model="username">
+        <label>Password</label>
+        <input v-model="password">
+        <button>Sign In</button>
+    </form>
+</template>
