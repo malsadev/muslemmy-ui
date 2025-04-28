2c51e1fa6323b7e7152a3d9f9da7410be682fcbd
malsadev <malsa080@uottawa.ca>
2025-04-27 21:50:30 -0400

    implemented admin registration view

---
+++ src/components/AdminRegistration.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { lemmyClient } from '@/services/lemmyService';
+import { ref } from 'vue';
+
+
+// lemmyClient.register({
+//     username: ""
+// });
+const username = ref('')
+const password = ref('')
+const vpassword = ref('')
+const answer = ref('')
+
+async function submit_admin_registration() {
+    const res = await lemmyClient.register({username: username.value,
+                                      password: password.value,
+                                      password_verify: vpassword.value,
+                                      answer: answer.value,
+    });
+
+    console.log(res)
+
+};
+
+</script>
+
+<template>
+
+    <h1>Admin Registration</h1>
+    <form @submit.prevent="submit_admin_registration">
+        <label>Username</label>
+        <input v-model="username">
+        <label>Password</label>
+        <input v-model="password">
+        <label>Password Verify</label>
+        <input v-model="vpassword">
+        <label>answer</label>
+        <input v-model="answer">
+        <button>Register</button>
+    </form>
+
+</template>
