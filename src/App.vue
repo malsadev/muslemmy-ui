d86dc94f030403c9360796e1ae621d1c1f9ff698
malsadev <malsa080@uottawa.ca>
2025-04-29 18:52:59 -0400

    implemented basic navbar

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,23 @@
 </script>
 
 <template>
+    <nav>
+        <RouterLink to="/">Site name</RouterLink>
+        <RouterLink to="/Communities">Communities</RouterLink>
+        <RouterLink to="/CreatePost">Create Post</RouterLink>
+        <RouterLink to="/CreateCommunity">Create Community</RouterLink>
+        <!-- Only when Admin is signed in -->
+        <RouterLink to="/AdminDashboard">Setting</RouterLink>
+        <label for="dropdown">Username</label>
+        <select id="dropdown" name="dropdown">
+            <option value="option1">Settings</option>
+            <option value="option2">Log out</option>
+        </select>
+
+    </nav>
     <main>
         <RouterView></RouterView>
     </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
