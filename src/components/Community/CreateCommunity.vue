3759d1a17e1629834046bb86ae28ad5481dde9b6
malsadev <malsa080@uottawa.ca>
2025-04-29 21:40:44 -0400

    implemented create post and create community views

---
+++ src/components/Community/CreateCommunity.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import { lemmyClient } from '@/services/lemmyService';
+
+const community_name = ref('');
+const community_title = ref('');
+
+async function submit_community() {
+    const res = await lemmyClient.createCommunity({
+        name: community_name.value,
+        title: community_title.value,
+    });
+
+    console.log(res);
+}
+
+
+</script>
+
+<template>
+    <form @submit.prevent>
+        <label>Community Name</label>
+        <input v-model="community_name">
+        <label>Community Title</label>
+        <input v-model="community_title">
+        <button>Create Community</button>
+    </form>
+</template>
---
+++ src/components/Post/CreatePost.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import { lemmyClient } from '@/services/lemmyService';
+
+const post_name = ref('');
+const community_id = ref('');
+
+async function submit_post() {
+    console.log(localStorage.getItem("lemmy_token"))
+    const res = await lemmyClient.createPost({
+        name: post_name.value,
+        community_id: community_id.value,});
+
+    console.log(res)
+    }
+</script>
+
+
+<template>
+
+    <form @submit.prevent="submit_post">
+        <label>Name</label>
+        <input v-model="post_name">
+        <label>Community ID</label>
+        <input v-model="community_id">
+        <button>Post</button>
+    </form>
+
+</template>
