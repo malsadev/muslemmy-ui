1023dcd4ee1e60fcc6e5f07b6ae672cdaa3f0ba5
malsadev <malsa080@uottawa.ca>
2025-05-05 23:14:31 -0400

    installed bootstrap for vue.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import {createBootstrap} from 'bootstrap-vue-next'
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+createApp(App).use(createBootstrap()).use(router).mount('#app')
