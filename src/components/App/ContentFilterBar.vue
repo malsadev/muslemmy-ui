2dda708ba9ec6acc7a5dd267ad69601136979156
malsadev <malsa080@uottawa.ca>
2025-05-07 02:12:15 -0400

    implemented homefeed with various filters

---
+++ src/components/App/ContentFilterBar.vue
@@ -0,0 +1,24 @@
+<script setup>
+// This should emit events to the parent component which the parent compoenent then should use to customize the post listing
+
+</script>
+
+
+<template>
+    <div class="content-filter-bar d-inline">
+        <BButtonGroup aria-label="Content Type Selection">
+            <BButton @click="$emit('changeToPosts')">Posts</BButton>
+            <BButton @click="$emit('changeToComments')">Comments</BButton>
+        </BButtonGroup>
+    </div>
+
+    <div class="community-filter-bar d-inline">
+        <BButtonGroup aria-label="Community List Type Selection">
+            <BButton @click="$emit('changeToSubscribed')">Subscribed</BButton>
+            <BButton @click="$emit('changeToLocal')">Local</BButton>
+            <BButton @click="$emit('changeToAll')">All</BButton>
+        </BButtonGroup>
+    </div>
+
+
+</template>
--- src/components/HomeFeed.vue
+++ src/components/HomeFeed.vue
@@ -3,15 +3,66 @@ sort type neciistates an an api call but the view remains the same -->
 <!-- Also the path could math queries or options wi -->
 
 <script setup>
+import { lemmyClient } from '@/services/lemmyService';
+import { ref, watch} from 'vue';
+import CustomizeBar from './App/ContentFilterBar.vue';
+import PostListings from './Post/PostListings.vue';
 
+// How to deal with pagination cursors?
+const postContentType = "posts";
+const commentContentType = "comments";
 
+const localCommunityListType = "Local";
+const subscribedCommunityListType = "Subscribed"
+const allCommunityListType = "All"
+
+const posts = ref([]);
+const comments = ref([]);
+const currentContentType = ref(postContentType); // 'posts' or 'comments'
+const currentCommunityListType = ref(localCommunityListType)
+
+async function fetchPosts() { //Change browser URl to indicate current query?
+    console.log("fetching posts in ", currentCommunityListType.value)    
+    currentContentType.value = postContentType;
+    try {
+        const response = await lemmyClient.getPosts({
+            type_: currentCommunityListType.value,
+        })
+        posts.value = response.posts;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+    return posts;
+}
+
+// https://vuejs.org/guide/essentials/watchers.html
+watch(currentCommunityListType, () => {
+    fetchPosts();
+});
+
+async function fetchComments() {
+    console.log("fetching comments in ", currentCommunityListType.value)
+    currentContentType.value = commentContentType;
+    try {
+        const response = await lemmyClient.getComments();
+        comments.value = response.comments;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+    }
+}
+
+fetchPosts();
 
 </script>
 
 
-<template>
-
-
-    <!-- need  a v-for statement here -->
+<template >
+    <CustomizeBar @changeToPosts="fetchPosts" @changeToComments="fetchComments" 
+        @changeToLocal=" currentCommunityListType = localCommunityListType"
+        @changeToSubscribed=" currentCommunityListType = subscribedCommunityListType "
+        @changeToAll=" currentCommunityListType = allCommunityListType "/>
+    <PostListings v-if="currentContentType == 'posts'" :posts="posts"/>
+    <!-- TODO -->
+    <!-- <CommentListings v-else-if="currentContentType == comments" :comments="comments"/> -->
 
 </template>
