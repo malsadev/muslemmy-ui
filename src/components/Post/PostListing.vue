85c2bead55f2e31df4dc8a44031373ddd3f90983
malsadev <malsa080@uottawa.ca>
2025-05-07 02:11:53 -0400

    Implemented post listing view and post list

---
+++ src/components/Post/PostListing.vue
@@ -0,0 +1,32 @@
+<script setup>
+// define props
+
+defineProps({
+    title: String,
+    author: String,
+    datePosted: String,
+    content: String,
+    upvotes: Number,
+    downvotes: Number,
+})
+
+</script>
+
+<!-- implement upvote count, downvote count, and buttons on the post listing -->
+
+<template>
+
+<!-- Apply Active State on hover Only -->
+<BListGroupItem href="#" class="flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">{{ title }}</h5>
+      <small>{{ datePosted }}</small>
+    </div>
+    <p class="mb-1">
+        {{ content }}
+    </p>
+    <!-- We should put small buttons here -->
+    <small>Donec id elit non mi porta.</small>
+  </BListGroupItem>
+
+</template>
---
+++ src/components/Post/PostListings.vue
@@ -0,0 +1,25 @@
+<!-- We need this because post listings could be part of homefeed, community posts, user posts.. etc -->
+<script setup>
+import PostListing from './PostListing.vue';
+defineProps({
+    posts: {
+        type: Array,
+        required: true
+    }
+})
+
+
+
+</script>
+
+<template>
+
+    <BListGroup>
+        <template v-for="post in posts">
+            <!-- <h1>{{ post.post.name }}</h1> -->
+            <PostListing :title="post.post.name" :author="post.creator.name" :content="post.post.body"
+                :datePosted="post.post.published" :upvotes="post.post.upvotes" :downvotes="post.post.downvotes" />
+        </template>
+    </BListGroup>
+
+</template>
